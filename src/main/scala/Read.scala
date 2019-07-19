b5224e8fdfd48ad9c96573d1e02947f6ce3ba8fe
Deepak Kumar Lakshmipathi <deepakkumarlakshmipathi@Deepaks-MacBook-Pro.local>
2019-07-19 11:21:49 -0400

    src

--- src/main/scala/Read.scala
+++ src/main/scala/Read.scala
@@ -1,7 +1,13 @@
+import org.apache.spark.sql.SparkSession
+
 object Read {
 
 
   def main(args: Array[String]): Unit = {
     println("this works" )
+
+    val spark_ = SparkSession.builder().appName("Test_app").master("local").getOrCreate()
+    val x  = spark_.read.csv("/Users/deepakkumarlakshmipathi/Desktop/craigslist-carstrucks-data/craigslistVehicles.csv")
+    x.show(2)
   }
 }
